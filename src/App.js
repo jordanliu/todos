260e19be2a70956841d3a9da418300c45b24ce74
jordanliu <jordanxavierliu@gmail.com>
2019-12-19 23:20:22 -0500

    Basic UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import React, { Component } from 'react';
-import {
-  Layout, Typography
-} from 'antd';
+import React from 'react';
 import './App.css';
+import { Layout, Typography, Card, Input, Button, Icon } from 'antd';
 const { Title } = Typography;
 const { Header, Content } = Layout;
 
+function App() {
 
-class App extends Component {
-  render() {
-    return (
-      <Layout>
+  function handleEnter(){
+    console.log("test");
+  }
+
+  return (
+    <div className="App">
+       <Layout>
           <Header>
           <Title>todos</Title>
           </Header>
           <Content>
-
+            <Card>
+         <div>
+         <Input
+          className="global-search"
+          size="large"
+          style={{ width: '100%' }}
+          placeholder="Enter to do list item"
+          onPressEnter={handleEnter}
+          suffix={
+              <Button
+                className="search-btn"
+                style={{ marginRight: -12 }}
+                size="large"
+                type="primary"
+              >
+                <Icon type="plus" />
+              </Button>
+            }
+          />
+         </div>
+            </Card>
           </Content>
       </Layout>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;

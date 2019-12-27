1f44d1d8712506c77af19a38eb7f04de4e066212
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-27 22:17:08 +0000

    Deploy jordanliu/todos to github.com/jordanliu/todos.git:gh-pages

---
+++ precache-manifest.4151dc0eb1f3e2d0f73f575debb630a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8090db83f14ea24ed11",
+    "url": "/todos/static/css/main.b14a6ea0.chunk.css"
+  },
+  {
+    "revision": "b8090db83f14ea24ed11",
+    "url": "/todos/static/js/main.f3073592.chunk.js"
+  },
+  {
+    "revision": "89246cf4ecee7b6195cb",
+    "url": "/todos/static/js/runtime~main.2b8fbda7.js"
+  },
+  {
+    "revision": "612e81b50ef3cd6e2271",
+    "url": "/todos/static/css/2.5b0a21cd.chunk.css"
+  },
+  {
+    "revision": "612e81b50ef3cd6e2271",
+    "url": "/todos/static/js/2.19574b11.chunk.js"
+  },
+  {
+    "revision": "9dd8cccb227562c3d11421a590956601",
+    "url": "/todos/index.html"
+  }
+];
